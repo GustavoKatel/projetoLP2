5323e141309838a4c879d2956ccfab934d26b4bd
GustavoKatel <gustavobs.katel@gmail.com>
2013-09-09 22:33:04 -0300

    TesteClient [lat|lon]_step aleatório

--- src/br/ufpb/lp2/ccarro/testes/TesteClient.java
+++ src/br/ufpb/lp2/ccarro/testes/TesteClient.java
@@ -37,23 +37,23 @@ public class TesteClient extends Thread {
 			//double lat = -7.135855, lon = -34.842932;
 			float lat = rand(r, -90,90), lon = rand(r, -180,180);
 			float lat_c = -7.135855f, lon_c = -34.842932f;
-			float lat_step = (lat-lat_c) / 500f;
-			float lon_step = (lon-lon_c) / 500f;
+			float lat_step = (lat-lat_c) / 1000000f;
+			float lon_step = (lon-lon_c) / 1000000f;
 			
 			System.out.println(name+": lat_step: "+lat_step+" lon_step: "+lon_step);
 			
 			while(true)
 			{
-				lat_c+=lat_step;
-				lon_c+=lon_step;
+				lat_c+=( r.nextInt(2)==0 ? lat_step : 0 );
+				lon_c+=( r.nextInt(2)==0 ? lon_step : 0 );
 				System.out.println(name+": lat: "+lat_c+"/"+lat+" lon_c: "+lon_c+"/"+lon);
 				//
 				if(Math.abs(lat_c)>=Math.abs(lat) || Math.abs(lon_c)>=Math.abs(lon))
 				{
 					lat = rand(r, -90,90);
 					lon = rand(r, -180,180);
-					lat_step = (lat-lat_c) / 500f;
-					lon_step = (lon-lon_c) / 500f;
+					lat_step = (lat-lat_c) / 1000000f;
+					lon_step = (lon-lon_c) / 1000000f;
 					System.out.println(name+": lat_step: "+lat_step+" lon_step: "+lon_step);
 				}
 				//
